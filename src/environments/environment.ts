e1c97d83cb090bd2e74607a22b1f10490b006184
lele79 <porcu.lele@tiscali.it>
2021-09-06 19:21:32 +0200

    [feat] init config firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,24 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
+import  *  as  keys  from  '../../secret.json';
 
+console.log('keys', keys)
+const  firebaseConfigKey: any = (keys as any).default
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: firebaseConfigKey.API_KEY,
+    authDomain: firebaseConfigKey.PROJECT_ID_DOMAIN,
+    databaseURL: firebaseConfigKey.PROJECT_ID_LINK,
+    projectId: firebaseConfigKey.PROJECT_ID,
+    storageBucket: firebaseConfigKey.PROJECT_ID_BUCKET,
+    messagingSenderId: firebaseConfigKey.SENDER_ID,
+    appId: firebaseConfigKey.APP_ID,
+    measurementId: firebaseConfigKey.G_MEASUREMENT_ID
+  }
 };
-
+console.log(environment.firebaseConfig)
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
